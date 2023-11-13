d126b56fad5cbc1ae425d01c3c93075498a49a3a
Sihle Mbobo <khwezimbobo@gmail.com>
2023-11-13 22:38:36 +0200

    made some changes

--- BMIcalc.js
+++ BMIcalc.js
@@ -1,14 +1,18 @@
-        function calculateBMI() {
-            const weight = parseFloat(document.getElementById('weight').value);
-            const height = parseFloat(document.getElementById('height').value);
+function calcBMI() {
+    // makes sure the variable called from id 'weight' is a number
+    let weight = parseFloat(document.getElementById('weight').value);
+    // makes sure the variable called from id 'height' is a number
+    let height = parseFloat(document.getElementById('height').value);
+// weight or height has no value or it is smaller than or equal to 0 send an alert
+    if (isNaN(weight) || isNaN(height) || height <= 0 || weight <= 0) {
+        alert('Please enter valid values for weight and height.');
+        return;
+    }
+// creates variable that calculates via BMI formula
+    let bmi = weight / (height * height);
+    // makes sure new variable output is rounded off to 2 decimal places 
+    let roundedBMI = bmi.toFixed(2);
 
-            if (isNaN(weight) || isNaN(height) || height <= 0 || weight <= 0) {
-                alert('Please enter valid values for weight and height.');
-                return;
-            }
-
-            const bmi = weight / (height * height);
-            const roundedBMI = bmi.toFixed(2);
-
-            document.getElementById('result').value = roundedBMI;
-        }
+    // fetches output from latest varible and adds it to the result id
+    document.getElementById('result').value = roundedBMI;
+}
